7b124b6c4bcddaf71a799607802faa187de3a845
Alhadji Oumate <alhadjioum0@gmail.com>
2024-08-03 23:21:54 +0100

    refactor: restructure of the backend environment requirements

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+Atlas backend Project
+
+## Intructions for local hosting of the backend
+
+To run the development environment run the script `npm run dev`
+To make the project ready to deploy in the distribution folder run `npm build`
+To run the ready-to-deploy folder run `npm start`
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,10 @@
-
-import express from 'express';
-
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello + Node.js + Express! with typscript and some updates');
 });
 
