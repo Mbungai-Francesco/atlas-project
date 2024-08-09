08f2dcc267f68a65a0e846558877c6a8ff83da07
Alhadji Oumate <alhadjioum0@gmail.com>
2024-08-09 01:21:35 +0100

    fix: correct response of user and teacher controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,29 +1,20 @@
-import express, {Request,Response} from 'express';
-require('dotenv').config()
-require('../routes/UserRoutes')
-import UserRoutes from '../routes/UserRoutes'
-import TeacherRoutes from "../routes/TeacherRoutes"
+import express, { Request, Response } from 'express';
+require('dotenv').config();
+require('../routes/UserRoutes');
+import UserRoutes from '../routes/UserRoutes';
+import TeacherRoutes from '../routes/TeacherRoutes';
 
+const app = express();
 
+app.use(express.json());
 
-const app = express()
+app.get('/', (req: Request, res: Response) => {
+  res.json('hello coming from the atlas landing page');
+});
 
-app.use(express.json())
-
-app.get('/', (req:Request, res:Response) =>{
-
-  res.send("hello coming from the atlas landing page")
-
-})
-
-app.use(UserRoutes,TeacherRoutes)
+app.use('/api', UserRoutes, TeacherRoutes);
 // app.use(UserRoutes,TeacherRoutes, /**.Routes */)
 
-
-
-
 app.listen(5000, () => {
-  console.log('server running on port 5000 : http://localhost:5000')
-})
-
-
+  console.log('server running on port 5000 : http://localhost:5000');
+});
