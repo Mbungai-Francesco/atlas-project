afa8e5d2af5b78e79a30982a18e4e664752539e1
Alhadji Oumate <alhadjioum0@gmail.com>
2024-08-12 02:54:34 +0100

    feat: adapting the routes to the newly created controllers

--- backend/routes/ClassroomRoutes.ts
+++ backend/routes/ClassroomRoutes.ts
@@ -4,6 +4,8 @@ import {
   GetClassrooms,
   GetMyClassrooms,
   GetClassroom,
+  UpdateClassroom,
+  DeleteClassroom,
 } from '../controllers/ClassroomController';
 
 const Classroomroutes = express.Router();
@@ -12,5 +14,7 @@ Classroomroutes.post('/classrooms', CreateClassroom);
 Classroomroutes.get('/classrooms', GetClassrooms);
 Classroomroutes.get('/myclassrooms', GetMyClassrooms);
 Classroomroutes.get('/classrooms/:id', GetClassroom);
+Classroomroutes.put('/classrooms/:id', UpdateClassroom);
+Classroomroutes.delete('/classrooms/:id', DeleteClassroom);
 
 export default Classroomroutes;
