b50cab029ec32034150c2ae5401ac6705665d837
Alhadji Oumate <alhadjioum0@gmail.com>
2024-08-14 23:11:29 +0100

    refactor: not found routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,11 +13,14 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.json('hello coming from the atlas landing page');
+  res.json({ message: 'Welcome to ATLAS API services...' });
 });
 
 app.use('/api', UserRoutes, TeacherRoutes, Classroomroutes, LoginRoutes);
-// app.use(UserRoutes,TeacherRoutes, /**.Routes */)
+
+app.get('*', (req: Request, res: Response) => {
+  res.status(404).json({ message: 'You are OUT OF BOUNDARIES!!!' });
+});
 
 app.listen(5000, () => {
   console.log('server running on port 5000 : http://localhost:5000');
